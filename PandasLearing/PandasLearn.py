2a47affcd1cb505a6e77641687d3436de331daef
yingzwei <35106733+yingzwei@users.noreply.github.com>
2018-01-06 18:04:06 +0800

    1

--- PandasLearing/PandasLearn.py
+++ PandasLearing/PandasLearn.py
@@ -6,11 +6,12 @@ from sklearn.externals.six import StringIO
 
 data = pd.read_csv('tree.csv')
 # print(data.head(0))
+heads = data.columns
 
 featureList = []
 labelList = []
 
-for row in data:
+for row in data.values:
     labelList.append(row[len(row)-1])
 
 print(labelList)    
