f89f67ead95a60260fedf37a7ad0faea2142aa60
yingzwei <Administrator@PC201604272122>
2018-01-06 21:46:48 +0800

    1

--- PandasLearing/PandasLearn.py
+++ PandasLearing/PandasLearn.py
@@ -4,6 +4,7 @@ from sklearn import tree
 from sklearn.externals.six import StringIO
 from sklearn.feature_extraction import DictVectorizer
 from numba.tests.test_deprecations import dummy
+from statsmodels.genmod.tests.results.results_glm_poisson_weights import predicted
 
 
 data = pd.read_csv('tree.csv')
@@ -24,8 +25,29 @@ print(featureList)
 vec = DictVectorizer()
 dummyX = vec.fit_transform(featureList).toarray()
 
-
 print('dummyX:' + str(dummyX))
 print(vec.get_feature_names())
 
 # vectorize class labels
+lb = preprocessing.LabelBinarizer()
+dummyY = lb.fit_transform(labelList)
+print('dummyY:' + str(dummyY))
+
+# using decision tree for classfication
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+clf = clf.fit(dummyX,dummyY)
+print('clf' + str(clf))
+
+with open("allElectronicInformationGainOri.dot", 'w') as f:
+    f= tree.export_graphviz(clf, feature_names=vec.get_feature_names(),out_file=f)
+
+oneRowx = dummyX[0,:]
+print('oneRowX:' + str(oneRowx))
+
+newRowX = oneRowx
+newRowX[0] = 1
+newRowX[1] = 0
+print('newRowX:' + str(newRowX))
+
+predictedY = clf.predict(newRowX)
+print('predictedY:' + str(predictedY))
