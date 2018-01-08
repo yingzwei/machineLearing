96887acb71693bf0d916ce436abfd79c88eef3d5
yingzwei <35106733+yingzwei@users.noreply.github.com>
2018-01-08 23:02:12 +0800

    1

--- MultipleReression/MultipleReressionTest.py
+++ MultipleReression/MultipleReressionTest.py
@@ -1,5 +1,22 @@
-import num as np
+import numpy as np
 import pandas as pd
 from sklearn import datasets, linear_model
 
-data = pd.read_csv() 
+data = pd.read_csv('MultipleLinerReression.csv') 
+
+X = data.values[:,:2]
+Y = data.values[:,-1]
+print(X)
+print(Y)
+
+regr= linear_model.LinearRegression()
+regr.fit(X, Y)
+print('coefficients:')
+print(regr.coef_)
+print('intercept:')
+print(regr.intercept_)
+
+xPred = [102,6]
+yPred = regr.predict([xPred])
+print('Predicted y:')
+print(yPred)
