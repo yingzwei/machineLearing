211d2f3272316282b5a35cfb004de2a73539ef80
yingzwei <35106733+yingzwei@users.noreply.github.com>
2018-01-16 16:29:17 +0800

    1

---
+++ test/A08_HotMap.py
@@ -0,0 +1,16 @@
+'''
+Created on 2018年1月16日
+热力图
+@author: Administrator
+'''
+import pandas as pd
+from pandas import DataFrame as df
+import matplotlib.pyplot as plt
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data'
+data = pd.read_csv(url, header=None ,prefix= 'V')
+
+corMat = df(data.corr())
+print(corMat)
+plt.pcolor(corMat)
+plt.show()
